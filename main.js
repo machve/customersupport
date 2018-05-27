0dc4089d7b0e7d1b9328f6bb1f596484b7612d21
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-27 22:45:45 +0200

    styling

--- main.js
+++ main.js
@@ -21,31 +21,41 @@ function show(data) {
 
         console.log(customer.first)
         let clone = template.cloneNode(true);
-
-
+        let dot = clone.querySelector('.dot');
+        let linkMore = clone.querySelector('.customers');
+        let more = clone.querySelector('.full-info-div');
+        let buttonRemove = clone.querySelector('.remove-button');
+        let arrow = clone.querySelector('.arrow');
 
         clone.querySelector('.customer-name').textContent = `${customer.first} ${customer.last}`;
         clone.querySelector('.place').textContent = customer.place;
         clone.querySelector('.message').textContent = customer.message;
-        clone.querySelector('.full').textContent = customer.full;
+        clone.querySelector('p.message-text').textContent = customer.full;
+        clone.querySelector('.time').textContent = `${customer.time.day}/${customer.time.month}/${customer.time.year}`
 
+        if (customer.importance >= 70) {
+            dot.style.backgroundColor = "#EE5164"
+        } else if (customer.importance < 70 && customer.importance > 40) {
+            dot.style.backgroundColor = "blue"
+        } else {
+            dot.style.backgroundColor = "#84fab0";
+        }
 
-        let buttonMore = clone.querySelector('.details-button');
-        let more = clone.querySelector('p');
-        let buttonRemove =clone.querySelector('.remove-button');
-
-        buttonMore.addEventListener('click', function () {
+        linkMore.addEventListener('click', function () {
 
             more.classList.toggle('hide');
+            linkMore.classList.toggle('extra-padding');
+            arrow.classList.toggle('collapsed');
+
         });
 
         buttonRemove.addEventListener('click', function () {
 
-            this.parentElement.remove();
+            more.parentElement.remove();
         });
 
         section.appendChild(clone);
-    })
+    });
 
 }
 
